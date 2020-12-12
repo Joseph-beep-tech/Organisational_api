3fa45209baa829ed5eaec2400fb3bc08c72358b5
Joseph-beep-tech <Josephcluff006@gmail.com>
2020-12-12 21:03:07 +0300

    getters and setters

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -12,4 +12,38 @@ public class User{
             this.position = position;
             this.role = role;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+
 }
