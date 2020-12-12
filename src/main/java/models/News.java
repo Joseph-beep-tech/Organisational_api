8032880707c02168e3d826bc1893486dd84a4c21
Joseph-beep-tech <Josephcluff006@gmail.com>
2020-12-12 20:19:26 +0300

    classes created

---
+++ src/main/java/models/News.java
@@ -0,0 +1,15 @@
+package models;
+
+import java.security.Timestamp;
+
+public class News{
+    private int id;
+    private int userid;
+    private String type;
+    private String content;
+    private Timestamp postdate;
+
+    public News(){
+
+    }
+}
