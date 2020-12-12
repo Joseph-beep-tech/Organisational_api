848df008582ae9bc042e18950e8767772dcbc90e
Joseph-beep-tech <Josephcluff006@gmail.com>
2020-12-12 20:26:20 +0300

    getters and setters generated

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -16,4 +16,46 @@ public class News{
             this.type = type;
             this.postdate = postdate;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getPostdate() {
+        return postdate;
+    }
+
+    public void setPostdate(Timestamp postdate) {
+        this.postdate = postdate;
+    }
+
+
 }
