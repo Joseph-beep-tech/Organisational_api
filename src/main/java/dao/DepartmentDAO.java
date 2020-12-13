9fb1c8162cb912f9d151441c5b538fc5fa9e9d70
Joseph-beep-tech <Josephcluff006@gmail.com>
2020-12-13 11:21:00 +0300

    Array lists

--- src/main/java/dao/DepartmentDAO.java
+++ src/main/java/dao/DepartmentDAO.java
@@ -9,4 +9,10 @@ import java.util.Objects;
 
 public interface DepartmentDAO{
     List<Department> getAllDepartments();
+    List<DepartmentNews> getDepartmentNewsById(int id);
+    List<User> getDepartmentUserById(int id);
+    void addDepartment (Department department);
+    Department findDepartmentById(int id);
+    void upDateDepartment(Department department, String name, String description);
+    void clearAllDepartments();
 }
