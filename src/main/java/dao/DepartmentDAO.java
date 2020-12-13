459627cb976154abb503932a7f3663dd8326165a
Joseph-beep-tech <Josephcluff006@gmail.com>
2020-12-13 22:44:10 +0300

    crud implimenntation

--- src/main/java/dao/DepartmentDAO.java
+++ src/main/java/dao/DepartmentDAO.java
@@ -11,8 +11,14 @@ public interface DepartmentDAO{
     List<Department> getAllDepartments();
     List<DepartmentNews> getDepartmentNewsById(int id);
     List<User> getDepartmentUserById(int id);
+
+    List<User> getDepartmentUsersById(int id);
+
     void addDepartment (Department department);
     Department findDepartmentById(int id);
     void upDateDepartment(Department department, String name, String description);
+
+    void updateDepartment(Department department, String name, String description);
+
     void clearAllDepartments();
 }
--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -9,5 +9,8 @@ public interface UserDAO{
     void addUser(User user);
     User findUserById(int id);
     void updateUser(int departmentId, String position, String role, String name, User user);
+
+    void updateUser(User user, String name, String position, String role, int departmentId);
+
     void clearAllUsers();
 }
